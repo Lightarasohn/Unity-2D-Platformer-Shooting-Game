b1aa65c78fa2505d9265c2c1d74a409489503113
Emre Çalışkan <114865537+emrecaliskan1@users.noreply.github.com>
2024-03-18 14:45:06 +0300

    Animasyonlar düzeltildi
    Daha yüksek pixel değerleriyle değiştirildi.

---
+++ 2DPlatformer/Assets/Scripts/GunPick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPick : MonoBehaviour
+{
+    private Sprite oldSpriteRenderer;
+    public Weapons currentWeapon;
+    private SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        currentWeapon = new Scar();
+
+        spriteRenderer.sprite = currentWeapon.getSprite();
+
+        oldSpriteRenderer = transform.GetComponent<SpriteRenderer>().sprite;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
