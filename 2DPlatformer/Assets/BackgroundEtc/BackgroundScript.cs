36bd4d82a9b89d93f78b74eb4bf50e9372375dfa
Volkan Çelebi <114937543+volkanchi@users.noreply.github.com>
2024-03-26 10:49:15 +0300

    örnek map tasarımı yapıldı
    Zıplamadan hemen sonra yüründüğünde animasyon bug ı oluşuyor

---
+++ 2DPlatformer/Assets/BackgroundEtc/BackgroundScript.cs
@@ -0,0 +1,37 @@
+
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    private float _startingPos; // baslangic pozisyonu
+    private float _lengthOfSprites; //sprite'larin uzunlugu
+    public float speedOfParallax; // parallax efektinin hizi
+    public Camera MainCamera; // ana kamera
+
+    private void Start()
+    {
+        _startingPos = transform.position.x; // sprite'in baslangic x pozisyonu aliniyor
+        _lengthOfSprites = GetComponent<SpriteRenderer>().bounds.size.x;    // sprite'larin uzunlugu aliniyor
+    }
+    private void Update()
+    {
+        Vector3 Position = MainCamera.transform.position;
+        float Temp = Position.x * (1 - speedOfParallax);
+        float Distance = Position.x * speedOfParallax;
+
+        Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
+
+        transform.position = NewPosition;
+
+        // arka plan tekarari\\
+        if (Temp > _startingPos + (_lengthOfSprites / 2))
+        {
+            _startingPos += _lengthOfSprites;
+        }
+        else if (Temp < _startingPos - (_lengthOfSprites / 2))
+        {
+            _startingPos -= _lengthOfSprites;
+        }
+    }
+
+}
