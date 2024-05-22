954732ccb869d4d2cd30e925d19c0020166549ce
Onur Demir <114931736+Lightarasohn@users.noreply.github.com>
2024-05-22 19:37:04 +0300

    Karakterin ve Yakıncı düşmanın ölümü ayarlandı
    ++Karakter öldüğünde Menu sahnesine gidiyoruz
    ++Yakıncı düşman artık ölebiliyor
    -----Yapılacaklar-----
    --Düşmanların canları kafalarının üstünde gözükebilmeli ve bu olay düşmanların ölmesi ile yok olabilmeli
    --Karakterin canı ekranın bir köşesinde gözükmeli
    --Oyun sırasında oyunu durdurabilmeliyiz
    --Karakter öldüğünde bizi menü dışında başka bir sahneye atmalı

--- 2DPlatformer/Assets/Scripts/MeleeEnemyScript.cs
+++ 2DPlatformer/Assets/Scripts/MeleeEnemyScript.cs
@@ -26,6 +26,10 @@ public class MeleeEnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (enemy.isDying())
+        {
+            GameObject.Destroy(gameObject);
+        }
         
         if (isAgroed || enemy.isAgro(transform))
         {
